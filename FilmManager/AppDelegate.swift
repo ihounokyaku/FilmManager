8865829304a827caa802b3f373e4a732c6e32789
Ihounokyaku <kamisama42@gmail.com>
2019-07-03 16:57:12 +0700

    Initial Commit

---
+++ FilmManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FilmManager
+//
+//  Created by Dylan Southard on 2019/07/03.
+//  Copyright © 2019 Dylan Southard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
